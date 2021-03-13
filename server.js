94aa94d26ac0500a49202d4c7cbe5b926875358f
Nikstar-1 <rk_787@hotmail.co.uk>
2021-03-13 18:56:45 +0000

    my first commit

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const port = process.env.PORT || 3001;
-const app = express();
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.listen(port, () => {
-  console.log('CRA server running on port', port);
-});
