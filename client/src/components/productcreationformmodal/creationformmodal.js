a2d66b6d0569db0b6669861ad92e2a8f368956a8
robertsicher <62100753+robertsicher@users.noreply.github.com>
2021-03-13 13:21:37 +0000

    Front end pages added

---
+++ client/src/components/productcreationformmodal/creationformmodal.js
@@ -0,0 +1,34 @@
+import {Button, Modal, } from 'react-bootstrap'
+import React, { useState } from "react";
+
+function Productcreationmodal() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <Button variant="primary" className="mx-2" onClick={handleShow}>
+          Add a new product
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+  
+  export default Productcreationmodal;
